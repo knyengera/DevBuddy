2b74a877c6f98a6607bc81504da9051fba7e511d
Kudakwashe Nyengera <kuda@fluff.software>
2024-11-12 02:01:57 +0200

    added bottom navigation

--- calmstack/app/index.tsx
+++ calmstack/app/index.tsx
@@ -2,19 +2,52 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import WelcomeScreen from './WelcomeScreen';
 import SignUpScreen from './SignUpScreen';
 import LogInScreen from './LogInScreen';
 import ForgotPasswordScreen from './ForgotPasswordScreen';
-import HomeScreen from './HomeScreen';
 import ChatbotScreen from './ChatbotScreen';
 import QuestsScreen from './QuestsScreen';
-import QuestDetailScreen from './QuestDetailScreen';
 import ProfileScreen from './ProfileScreen';
+import QuestDetailScreen from './QuestDetailScreen'; // If needed
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+// Define the Tab Navigator
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false, // Hide header if desired
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Chatbot') {
+            iconName = 'chatbubbles-outline';
+          } else if (route.name === 'Quests') {
+            iconName = 'list-outline';
+          } else if (route.name === 'Profile') {
+            iconName = 'person-outline';
+          } else {
+            iconName = 'default-icon'; // Provide a default icon name
+          }
+
+          // Return the icon component
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#4CAF50',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Chatbot" component={ChatbotScreen} />
+      <Tab.Screen name="Quests" component={QuestsScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -23,12 +56,9 @@ export default function App() {
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="LogIn" component={LogInScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-        <Stack.Screen name="QuestDetail" component={QuestDetailScreen} />
-        <Stack.Screen name="Quests" component={QuestsScreen} />
-        <Stack.Screen name="Chatbot" component={ChatbotScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Home" component={TabNavigator} /> 
+        <Stack.Screen name="QuestDetail" component={QuestDetailScreen} /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
